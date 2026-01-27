d20d06300e976afb967e81420d4f75b1eb43dc7a
ADMIN <tranquangtruong25@gmail.com>
2026-01-28 00:05:40 +0700

    update jest setup

--- englishApp/babel.config.js
+++ englishApp/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
     api.cache(true);
+    const isTest = api.env('test');
     return {
-        presets: ['babel-preset-expo'],
+        presets: [
+            isTest ? 'module:metro-react-native-babel-preset' : 'babel-preset-expo'
+        ],
         plugins: [
             ['@babel/plugin-transform-flow-strip-types', { allowDeclareFields: true }]
         ]
