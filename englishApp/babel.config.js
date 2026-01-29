3cd94121c54675b29c97a98870d9d52e1da250b8
ADMIN <tranquangtruong25@gmail.com>
2026-01-29 22:28:28 +0700

    fix: force flow transformation for node_modules using babel overrides

--- englishApp/babel.config.js
+++ englishApp/babel.config.js
@@ -1,13 +1,18 @@
 module.exports = function (api) {
-    const isTest = api.env('test');
-    api.cache.using(() => isTest);
+    api.cache(true);
     return {
-        presets: [
-            isTest ? 'module:metro-react-native-babel-preset' : 'babel-preset-expo',
-            '@babel/preset-flow'
-        ],
+        presets: ['babel-preset-expo'],
         plugins: [
+            ['@babel/plugin-syntax-flow', { all: true }],
             ['@babel/plugin-transform-flow-strip-types', { allowDeclareFields: true, all: true }]
+        ],
+        overrides: [
+            {
+                test: /[\\/]node_modules[\\/]/,
+                plugins: [
+                    ['@babel/plugin-transform-flow-strip-types', { allowDeclareFields: true, all: true }]
+                ]
+            }
         ]
     };
 };
